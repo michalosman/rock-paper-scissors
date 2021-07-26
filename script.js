7b4e4cc1432124772129a78776e79b6e5a0aea34
michalosman <michal.osman.997@gmail.com>
2021-07-26 19:51:20 +0200

    Add endgame modal & Refactor code

--- script.js
+++ script.js
@@ -1,86 +1,28 @@
+// GAME
+
 let playerScore = 0
 let computerScore = 0
+let roundWinner = ''
 
-const signButtons = document.querySelectorAll('.btn')
-signButtons.forEach((button) => button.addEventListener('click', playRound))
-
-function playRound(e) {
-  if (isGameOver()) {
-    alert('Game over, click F5 to play again')
-    return
-  }
-
-  let buttonId
-  if (e.target.tagName.toLowerCase() === 'i') {
-    buttonId = e.target.parentNode.id
-  } else {
-    buttonId = e.target.id
-  }
-
-  const playerSelection = convertIdToSign(buttonId)
-  const computerSelection = getRandomChoice()
-  updateScore(getWinner(playerSelection, computerSelection))
-  updateChoices(playerSelection, computerSelection)
-}
-
-function updateScore(winner) {
-  const scoreInfo = document.getElementById('score-info')
-  const playerScorePara = document.getElementById('player-score')
-  const computerScorePara = document.getElementById('computer-score')
-
-  if (winner === 'tie') {
-    scoreInfo.textContent = "It's a tie!"
-  } else if (winner === 'player') {
-    scoreInfo.textContent = 'You won!'
-    playerScore++
-  } else if (winner === 'computer') {
-    scoreInfo.textContent = 'You lost!'
-    computerScore++
-  }
-
-  playerScorePara.textContent = `Player: ${playerScore}`
-  computerScorePara.textContent = `Computer: ${computerScore}`
-
-  if (isGameOver()) setFinalMessage()
-}
-
-function updateChoices(playerSelection, computerSelection) {
-  const playerSign = document.getElementById('player-sign')
-  const computerSign = document.getElementById('computer-sign')
-
-  playerSign.classList.add('active')
-  computerSign.classList.add('active')
-
-  const playerSignClassName = `fa-hand-${playerSelection.toLowerCase()}`
-  const computerSignClassName = `fa-hand-${computerSelection.toLowerCase()}`
-
-  playerSign.classList = `fas ${playerSignClassName} active`
-  computerSign.classList = `fas ${computerSignClassName} active`
-}
-
-function setFinalMessage() {
-  return playerScore > computerScore
-    ? (scoreHeading.textContent = 'Game over, you won')
-    : (scoreHeading.textContent = 'Game over, you lost')
-}
-
-function getWinner(playerSelection, computerSelection) {
+function playRound(playerSelection, computerSelection) {
   if (playerSelection === computerSelection) {
-    return 'tie'
+    roundWinner = 'tie'
   }
   if (
     (playerSelection === 'ROCK' && computerSelection === 'SCISSORS') ||
     (playerSelection === 'SCISSORS' && computerSelection === 'PAPER') ||
     (playerSelection === 'PAPER' && computerSelection === 'ROCK')
   ) {
-    return 'player'
+    playerScore++
+    roundWinner = 'player'
   }
   if (
     (computerSelection === 'ROCK' && playerSelection === 'SCISSORS') ||
     (computerSelection === 'SCISSORS' && playerSelection === 'PAPER') ||
     (computerSelection === 'PAPER' && playerSelection === 'ROCK')
   ) {
-    return 'computer'
+    computerScore++
+    roundWinner = 'computer'
   }
 }
 
@@ -96,10 +38,82 @@ function getRandomChoice() {
   }
 }
 
-function convertIdToSign(buttonId) {
-  return buttonId.split('-')[0].toUpperCase()
-}
-
 function isGameOver() {
   return playerScore === 5 || computerScore === 5
+}
+
+// UI
+
+const scoreInfo = document.getElementById('scoreInfo')
+const playerScorePara = document.getElementById('playerScore')
+const computerScorePara = document.getElementById('computerScore')
+const playerSign = document.getElementById('playerSign')
+const computerSign = document.getElementById('computerSign')
+const rockBtn = document.getElementById('rockBtn')
+const paperBtn = document.getElementById('paperBtn')
+const scissorsBtn = document.getElementById('scissorsBtn')
+const endgameModal = document.getElementById('endgameModal')
+const endgameMsg = document.getElementById('endgameMsg')
+const overlay = document.getElementById('overlay')
+const restartBtn = document.getElementById('restartBtn')
+
+rockBtn.addEventListener('click', () => handleClick('ROCK'))
+paperBtn.addEventListener('click', () => handleClick('PAPER'))
+scissorsBtn.addEventListener('click', () => handleClick('SCISSORS'))
+restartBtn.addEventListener('click', restartGame)
+
+function handleClick(playerSelection) {
+  const computerSelection = getRandomChoice()
+  playRound(playerSelection, computerSelection)
+  updateChoices(playerSelection, computerSelection)
+  updateScore()
+
+  if (isGameOver()) {
+    openEndgameModal()
+  }
+}
+
+function updateChoices(playerSelection, computerSelection) {
+  const playerSignClassName = `fa-hand-${playerSelection.toLowerCase()}`
+  const computerSignClassName = `fa-hand-${computerSelection.toLowerCase()}`
+
+  playerSign.classList = `fas ${playerSignClassName} active`
+  computerSign.classList = `fas ${computerSignClassName} active`
+}
+
+function updateScore() {
+  if (roundWinner === 'tie') {
+    scoreInfo.textContent = "It's a tie!"
+  } else if (roundWinner === 'player') {
+    scoreInfo.textContent = 'You won!'
+  } else if (roundWinner === 'computer') {
+    scoreInfo.textContent = 'You lost!'
+  }
+
+  playerScorePara.textContent = `Player: ${playerScore}`
+  computerScorePara.textContent = `Computer: ${computerScore}`
+}
+
+function openEndgameModal() {
+  endgameModal.classList.add('active')
+  overlay.classList.add('active')
+  setFinalMessage()
+}
+
+function setFinalMessage() {
+  return playerScore > computerScore
+    ? (endgameMsg.textContent = 'You won 😀')
+    : (endgameMsg.textContent = 'You lost 🙁')
+}
+
+function restartGame() {
+  playerScore = 0
+  computerScore = 0
+  scoreInfo.textContent = 'Score'
+  playerScorePara.textContent = 'Player: 0'
+  computerScorePara.textContent = 'Computer: 0'
+  playerSign.classList.remove('active')
+  computerSign.classList.remove('active')
+  endgameModal.classList.remove('active')
+  overlay.classList.remove('active')
 }
